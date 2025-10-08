db9d9e898e10ff3246a7f6e035a9cec4b0ddc138
Nicholasstiv1 <nicholas.guerino@gmail.com>
2025-10-08 00:39:14 -0400

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Login from './pages/Login/login';
 import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/header/header';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
+      <Header></Header>
       <Login />
+      <GlobalStyle />
     </>
   );
 }
