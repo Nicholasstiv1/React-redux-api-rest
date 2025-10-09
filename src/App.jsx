6899c62aca5a798cddffb5639dd21b7b63f1c4a3
Nicholasstiv1 <nicholas.guerino@gmail.com>
2025-10-08 22:50:58 -0400

    toastify config

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Header from './components/header/header';
 import AppRoutes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ToastContainer } from 'react-toastify';
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <AppRoutes />
       <GlobalStyle />
+      <ToastContainer autoClose={3000} className="toast-container" />
     </BrowserRouter>
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,7 +9,7 @@ export default function AppRoutes() {
       <Route
         path="/"
         element={
-          <MyRoute isClosed>
+          <MyRoute>
             <Login />
           </MyRoute>
         }
